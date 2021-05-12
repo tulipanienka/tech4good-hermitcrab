1f9109a57b2608122507663067488183e77eb44e
Bram <bramprovoost@gmail.com>
2021-05-12 10:39:47 +0200

    first commit

---
+++ Hermit_Crab/android/app/src/main/kotlin/com/example/Hermit_Crab/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Hermit_Crab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Hermit_Crab/lib/pages/select_home.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class SelectHome extends StatefulWidget {
+  @override
+  _SelectHomeState createState() => _SelectHomeState();
+}
+
+class _SelectHomeState extends State<SelectHome> {
+  @override
+  Widget build(BuildContext context) {
+    bool pressAttention = false;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Select Home"),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 64),
+              Text(
+                "Choose the type of house \nyou live in",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(height: 32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Container(
+                  //   padding: EdgeInsets.all(0.0),
+                  //   child: OutlinedButton(
+                  //     child: Image.asset(
+                  //       'assets/home1.png',
+                  //       height: 100,
+                  //     ),
+                  //     style: ButtonStyle(
+                  //       side: MaterialStateProperty.resolveWith((states) {
+                  //         Color _borderColor;
+
+                  //         if (states.contains(MaterialState.disabled)) {
+                  //           _borderColor = Colors.greenAccent;
+                  //         } else if (states.contains(MaterialState.pressed)) {
+                  //           _borderColor = Colors.yellow;
+                  //         } else {
+                  //           _borderColor = Colors.pinkAccent;
+                  //         }
+
+                  //         return BorderSide(color: _borderColor, width: 5);
+                  //       }),
+                  //     ),
+                  //     onPressed: () {
+                  //       print("klik");
+                  //     },
+                  //   ),
+                  // ),
+                  RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        pressAttention = !pressAttention;
+                      });
+                      sleep(Duration(seconds: 1));
+                      Navigator.pop(context);
+                    },
+                    padding: EdgeInsets.all(0.0),
+                    child: Image.asset(
+                      'assets/home1.png',
+                      height: 100,
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  RaisedButton(
+                    onPressed: () {
+                      sleep(Duration(seconds: 1));
+                      Navigator.pop(context);
+                    },
+                    padding: EdgeInsets.all(0.0),
+                    child: Image.asset(
+                      'assets/home2.png',
+                      height: 100,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () {
+                      sleep(Duration(seconds: 1));
+                      Navigator.pop(context);
+                    },
+                    padding: EdgeInsets.all(0.0),
+                    child: Image.asset(
+                      'assets/home3.png',
+                      height: 100,
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  RaisedButton(
+                    onPressed: () {
+                      sleep(Duration(seconds: 1));
+                      Navigator.pop(context);
+                    },
+                    padding: EdgeInsets.all(0.0),
+                    child: Image.asset(
+                      'assets/home4.png',
+                      height: 100,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () {
+                      sleep(Duration(seconds: 1));
+                      Navigator.pop(context);
+                    },
+                    padding: EdgeInsets.all(0.0),
+                    child: Image.asset(
+                      'assets/home5.png',
+                      height: 100,
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  RaisedButton(
+                    onPressed: () {
+                      sleep(Duration(seconds: 1));
+                      Navigator.pop(context);
+                    },
+                    padding: EdgeInsets.all(0.0),
+                    child: Image.asset(
+                      'assets/home6.png',
+                      height: 100,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
